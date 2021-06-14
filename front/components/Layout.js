7f4a7769149de8f2c5f65784ebe74083ebe70d41
Vincent HO <vincent.ho@agence-belle-epoque.fr>
2021-06-14 11:39:49 +0200

    showcase compo + images

--- front/components/Layout.js
+++ front/components/Layout.js
@@ -1,11 +1,17 @@
 import Head from 'next/head'
 
+import {useRouter} from 'next/router'
+
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+
+import Showcase from '@/components/Showcase'
 
 import styles from '@/styles/Layout.module.css'
 
 export default function Layout({title, keywords, description, children}) {
+  const router = useRouter()
+
   return (
     <div>
       <Head>
@@ -15,6 +21,7 @@ export default function Layout({title, keywords, description, children}) {
       </Head>
 
       <Header />
+      {router?.pathname === '/' && <Showcase />}
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
