3eddad4acdf0d56fd1ebd0f98854e9ba670b69d0
Vincent HO <vincent.ho@agence-belle-epoque.fr>
2021-06-16 11:40:48 +0200

    search page using filters

---
+++ front/pages/events/search.js
@@ -0,0 +1,48 @@
+import {useRouter} from 'next/router'
+import Link from 'next/link'
+
+import {API_URL} from '@/config/index'
+import qs from 'qs'
+
+import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
+
+import styles from '@/styles/Event.module.css'
+
+export default function SearchPage({events}) {
+  const router = useRouter()
+
+  return (
+    <Layout title="Search Results">
+      <Link href="/events">
+        <a className={styles.back}>{'<'} Go Back</a>
+      </Link>
+      <h1>Search Results for: {router?.query?.term}</h1>
+
+      {events?.length === 0 && <h3>No events to show</h3>}
+      {events?.map((evt) => (
+        <EventItem key={evt?.id} evt={evt} />
+      ))}
+    </Layout>
+  )
+}
+
+export async function getServerSideProps({query: {term}}) {
+  const query = qs.stringify({
+    _where: {
+      _or: [
+        {name_contains: term},
+        {performers_contains: term},
+        {description_contains: term},
+        {venue_contains: term},
+      ],
+    },
+  })
+
+  const response = await fetch(`${API_URL}/events?${query}`)
+  const events = await response.json()
+
+  return {
+    props: {events},
+  }
+}
